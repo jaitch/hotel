64f12c41b2cf154cbb45ee26e61c4c83dcc9798e
Janice Huang <janicehuang@gmail.com>
2019-09-03 13:25:12 -0700

    add requires to test_helper, make block, calendar, and reservation files and test files

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,14 @@
-# Add simplecov
+require 'simplecov'
+SimpleCov.start do
+  add_filter 'test'
+end
+
 require "minitest"
 require "minitest/autorun"
 require "minitest/reporters"
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
-# require_relative your lib files here!
+require_relative 'block'
+require_relative 'calendar'
+require_relative 'reservation'
