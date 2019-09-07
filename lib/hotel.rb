5d2e1409b54148dae578ac6557ad39cbcac79728
Janice Huang <janicehuang@gmail.com>
2019-09-07 12:12:05 -0700

    finish calculate cost function, instantiate date range in make_res function rather than in helper methods, start writing more tests

--- lib/hotel.rb
+++ lib/hotel.rb
@@ -24,15 +24,19 @@ module Hotel
     def make_reservation(start_date, end_date)
       @start_date = Date.parse(start_date)
       @end_date = Date.parse(end_date)
+      @pending_date_range = DateRange.new(start_date.to_s, end_date.to_s)
 
-      validate_dates(@start_date, @end_date)
-      find_available_room(@all_rooms, @start_date, @end_date)
-      # if find_available_room(@all_rooms, @start_date, @end_date) == true
-      # elsif find_available_room(@all_rooms, @start_date, @end_date) == false
+      validate_dates(@pending_date_range)
+
+      if find_available_room(@all_rooms, @pending_date_range) == true
+        return "Reservation booked. Amount due: $#{calculate_cost(@pending_date_range)}."
+      elsif find_available_room(@all_rooms, @pending_date_range) == false
+        return "Sorry. No available rooms for that date."
+      end
     end
 
     # helper method for validation
-    def validate_dates(start_date, end_date)
+    def validate_dates(pending_date_range)
       if Date.valid_date?(start_date.year,start_date.mon,start_date.mday) == false || Date.valid_date?(end_date.year,end_date.mon,end_date.mday) == false
         raise ArgumentError, "You must give valid dates."
       end
@@ -43,26 +47,33 @@ module Hotel
     end
 
     # helper method for finding room
-    def find_available_room(all_rooms, start_date, end_date)
-      other_date_range = DateRange.new(start_date.to_s, end_date.to_s)
+    def find_available_room(all_rooms, pending_date_range)
       all_rooms.each do |room|
         if room.occupied_date_ranges.length == 0
-          room.occupied_date_ranges << other_date_range
+          room.occupied_date_ranges << pending_date_range
           return true
         end
 
         room.occupied_date_ranges.each do |date_range|
-          if date_range.overlap?(other_date_range) == false
-            room.occupied_date_ranges << other_date_range
+          if date_range.overlap?(@pending_date_range) == false
+            room.occupied_date_ranges << @pending_date_range
             return true
           end
         end
       end
       return false
     end
-  end
 
+    # helper method for calculating cost
+    def calculate_cost(pending_date_range)
+      rate = 200
+      amount_due = rate * pending_date_range.duration
+      return amount_due
+    end
+
+  end
 end
+
 
 # calculate the cost of this reservation
 # return room number and cost
--- test/hotel_test.rb
+++ test/hotel_test.rb
@@ -22,8 +22,8 @@ describe 'make_reservation' do
   let (:hotel) {
     @new_hotel = Hotel::Hotel.new(5)
   }
-  it "makes a reservation" do
-    expect (hotel.make_reservation('2001-02-03', '2001-02-06')).must_equal true
+  it "makes a successful reservation and returns amount due" do
+    expect (hotel.make_reservation('2001-02-03', '2001-02-06')).must_include "Reservation booked. Amount due: $600."
   end
 
   it 'will skip over rooms if they are occupied for the given date' do
@@ -35,11 +35,19 @@ describe 'make_reservation' do
     expect(hotel.all_rooms[2].occupied_date_ranges.length).must_equal 1
     expect(hotel.all_rooms[3].occupied_date_ranges.length).must_equal 0
   end
-  it "adds the succesfully made reservation to the correct room's occupied_date_ranges array" do
+  it "returns apology msg instead of making the reservation if there are no available rooms" do
+    (hotel.make_reservation('2019-2-2', '2019-2-5'))
+    (hotel.make_reservation('2019-2-2', '2019-2-5'))
+    (hotel.make_reservation('2019-2-2', '2019-2-5'))
+    (hotel.make_reservation('2019-2-2', '2019-2-5'))
+    (hotel.make_reservation('2019-2-2', '2019-2-5'))
+    (hotel.make_reservation('2019-2-2', '2019-2-5'))
+    (hotel.make_reservation('2019-2-2', '2019-2-5'))
+    expect((hotel.make_reservation('2019-2-2', '2019-2-5'))).must_equal "Sorry. No available rooms for that date."
   end
 end
 
-describe "exception thrower" do
+describe 'exception thrower' do
   it "raises an ArgumentError if the end date is before the start date" do
     expect {Hotel::Hotel.new'2001-02-04', '2001-02-03'}.must_raise ArgumentError
   end
@@ -49,4 +57,14 @@ describe "exception thrower" do
   it "raises an ArgumentError if given an invalid end date" do
     expect {Hotel::Hotel.new'2001-03-14', '2001-02-29'}.must_raise ArgumentError
   end
+end
+
+describe 'available_dates' do
+it 'lists available rooms given a date'
+end
+end
+
+describe 'booked_reservations' do
+it 'lists booked reservations for a given date'
+end
 end
